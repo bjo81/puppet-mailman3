9fa1e695eaf4ff70c5a6a844d3a52d7500650b45
pctony <tony@pc-tony.com>
2015-05-28 23:36:17 +0100

    Start adding config options for mailman.cfg

---
+++ manifests/core/config.pp
@@ -0,0 +1,16 @@
+#/etc/puppet/modules/mailman3/manifests/core/config.pp
+
+# == Class mailman3::core::config
+#
+# === Parameters 
+#
+# ( As this list is so long it has been alpha sorted, to hopefully make it easier parse with the naked eye)
+#
+# [*siteowner*]
+#  This address is the "site owner" address.  Certain messages which must be
+#  delivered to a human, but which can't be delivered to a list owner (e.g. a
+#  bounce from a list owner), will be sent to this address.  It should point to
+#  a human.
+
+class mailman3::core::config (
+  $siteowner   = 'root@localhost', # Set a default address that should be safe.
