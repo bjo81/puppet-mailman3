4bc07a1c1152ee0d916469e29d442b5b18bfa583
pctony <tony@pc-tony.com>
2015-05-28 23:49:30 +0100

    Add more template variables for use in mailman.cfg

--- manifests/core/config.pp
+++ manifests/core/config.pp
@@ -6,6 +6,22 @@
 #
 # ( As this list is so long it has been alpha sorted, to hopefully make it easier parse with the naked eye)
 #
+# [*defaultlanguage*]
+#  The default language for this server.
+#
+# [*noreplyaddress*]
+#  This is the local-part of an email address used in the From field whenever a
+#  message comes from some entity to which there is no natural reply recipient.
+#  Mailman will append '@' and the host name of the list involved.  This
+#  address must not bounce and it must not point to a Mailman process.
+#
+# [*senderheaders*]
+#  Membership tests for posting purposes are usually performed by looking at a
+#  set of headers, passing the test if any of their values match a member of
+#  the list.  Headers are checked in the order given in this variable.  The
+#  value From_ means to use the envelope sender.  Field names are case
+#  insensitive.  This is a space separate list of headers.
+#
 # [*siteowner*]
 #  This address is the "site owner" address.  Certain messages which must be
 #  delivered to a human, but which can't be delivered to a list owner (e.g. a
@@ -13,7 +29,10 @@
 #  a human.
 
 class mailman3::core::config (
-  $siteowner   = 'root@localhost', # Set a default address that should be safe.
+  $defaultlanguage = 'en',
+  $noreplyaddress  = 'noreply', # See comment at the top for usage instructions.
+  $senderheaders   = 'from from_ reply-to sender',
+  $siteowner       = 'root@localhost', # Set a default address that should be safe.
 
 
 ) {
